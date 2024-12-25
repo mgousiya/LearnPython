2f9a1c3ecae4ec812ecaf9d4b7957eac2777007c
Gousiya <mgousiya@amazon.com>
2024-12-25 15:29:23 +0530

    revision

--- Learning/loops.py
+++ Learning/loops.py
@@ -1,7 +1,27 @@
-
-fruits = ["apple","bannana","grapes"]
+""" 
+"fruits = ["apple","bannana","grapes"]
 
 for anything in fruits:
     for _ in anything:
         print(_)
-    print (anything)
+    print (anything)" """
+
+""" fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+  if x == "banana":
+    continue """
+  
+""" fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  if x == "banana":
+    break
+  print(x) """
+
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  if x == "banana":
+    continue
+  print(x)
+  
--- Practise/practise1.py
+++ Practise/practise1.py
@@ -1,1 +1,54 @@
-print("my first code")
+# variable example
+""" sum = a + b
+print(sum) """
+
+# varibale with string
+
+""" email = "gousiya@email.com"
+print("email is " + email) """
+
+# variable with f string
+
+""" print(f"my email is {email}") """
+
+# float example
+""" age = int(input("Type your age here \n"))
+print(f"my age is {age}") """
+
+""" float = float(input())
+print(type(float))
+ """
+# Boolean Example
+
+""" is_iam_in_hyd = True
+
+print(type(is_iam_in_hyd)) """
+
+# Boolean with if else
+
+""" is_hyderabad = False
+
+if is_hyderabad:
+    print("Iam in hyd")
+else:
+    print("Not in hyd") """
+
+# another ex for boolean
+""" input  = "Summer"
+if input == "Winter" :
+    print("Winter")
+else:
+    print("Summer") """
+
+# If else practise with f string
+
+""" age = int(input("Type your age here \n"))
+if age > 30:
+    print(f"Go to Balcony beacause your age is {age}")
+elif age >= 20 and age <=30:
+    print(f"Go to lower Balcony beacause your age is {age}")
+elif age > 10 and age <= 20:
+    print(f"Go to Normal beacause your age is {age}")
+else:
+    print(f"Go to regular beacause your age is {age}")  """
+
