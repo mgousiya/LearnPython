32e6f7fbfd624443081e5d1f254b088f19320095
Gousiya <mgousiya@amazon.com>
2024-12-27 22:21:19 +0530

    whileloop

---
+++ Learning/1.While loops.py
@@ -0,0 +1,34 @@
+# while loop = execute some code WHILE some condition remains TRUE
+
+ ## Name Example ----
+""" name = input("Enter your name: ")
+while name == "":
+    print("You did not enter your name")
+    name = input("Enter your name: ")
+    print(f"Hello {name}")
+ """
+ ## Age Example -----
+ 
+""" age = int(input("Enter your age: "))
+while age < 0:
+    print("Age can't be negative")
+    age = int(input("Enter your age: "))
+    print(f"you are {age} years old")
+"""
+
+## Food Example ___ (using "not true" here)
+
+""" food = input("Enter your favourite food (q to quit): ")
+while not food == "q":
+    print(f"your favourite is {food}")
+    food = input("Enter your another favourite food (q to quit): ")
+print("Bye")
+ """
+
+## Number Example --- using "or operator "
+
+""" num = int(input("Select a num between 1-10: "))
+while num < 1 or num > 10:
+    print(f"{num} is not valid")
+    num = int(input("Select a num between 1-10: "))
+print(f"your num is {num}") """
--- Learning/loops.py
+++ Learning/loops.py
@@ -19,9 +19,25 @@ for x in fruits:
   print(x) """
 
 
-fruits = ["apple", "banana", "cherry"]
+""" fruits = ["apple", "banana", "cherry"]
 for x in fruits:
   if x == "banana":
     continue
   print(x)
-  
+ """
+
+data = [
+    {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+]
+
+
+
+new_list = []
+for item in data:
+  new_item = item["brand"]
+  new_list.append(new_item)
+  print(new_list)
