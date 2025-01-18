1b4293e11ef13fe75497df1542f51258fc56ae7f
Gousiya <mgousiya@amazon.com>
2025-01-18 21:23:03 +0530

    reading

--- 38_Reading Files/main.py
+++ 38_Reading Files/main.py
@@ -1,3 +1,14 @@
 #Python reading files (.txt, .json, .csv)
+# with-statement: The with statement is used to wrap  the execution of a block with 
+#                 methods defined by a context manager.
+# This allows common try.... except... finally usage patterns to be encapsulated for convinient reuse.
 
-file_path = "C:\Users\mgousiya\Desktop\input.txt"
+
+file_path = "C:\Users\HP\Desktop\input.txt"
+try:
+    with open(file_path, "r") as file:
+        content= file.read()
+        print(content)
+except FileNotFoundError:
+    print("That file was not found")
+
